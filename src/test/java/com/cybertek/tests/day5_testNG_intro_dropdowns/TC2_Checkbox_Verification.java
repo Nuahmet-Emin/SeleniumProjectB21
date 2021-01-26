883f5ef70ed03d90844055dee08bed7f13d39c01
nurahmet-azat <nurahmedemin@gmail.com>
2021-01-25 17:14:59 -0800

    day5-testNG

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TC2_Checkbox_Verification.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +18,24 @@ public class TC2_Checkbox_Verification {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         //verify"Success - Check box is checked" msg is not displayed
+        WebElement successMessage = driver.findElement(By.xpath("//div[@id ='txtAge'  ]"));
+
+        //locating check box
+        WebElement checkbox1 = driver.findElement(By.xpath("//input[@id='isAgeSelected']"));
+        if(!checkbox1.isSelected() && !successMessage.isDisplayed()){
+            System.out.println("Checkbox is not selected. Message is not displayed. Verification PASS!");
+        }else{
+            System.out.println("FAILED!!!!");
+        }
+  //click
+        checkbox1.click();
+        if(checkbox1.isSelected() && successMessage.isDisplayed()) {
+            System.out.println("Checkbox is  selected. Message is displayed. Verification PASS!");
+        }else{
+            System.out.println("FAILED!!!");
+        }
+
+        driver.close();
 
 
 
